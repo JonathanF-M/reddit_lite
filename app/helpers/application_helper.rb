b57cac6d327c053e97dc380f673eace6a0628e13
Jonathan Farias <jfariasm647@gmail.com>
2025-03-01 17:42:18 -0800

    format/style user login form on modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def devise_mapping
+    Devise.mappings[:user] # Change :user if you have another Devise model
+  end
+
+  def devise_resource
+    User.new
+  end
+
+  def devise_resource_name
+    :user
+  end
 end
