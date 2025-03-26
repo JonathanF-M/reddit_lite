0b22c77d5d3cacf13a1da86304af432ff8779c91
Jonathan Farias <jfariasm647@gmail.com>
2025-03-25 17:22:46 -0700

    style new comment form, save current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to @post
   end
 
   def destroy
@@ -14,6 +16,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :status)
+    params.require(:comment).permit(:body)
   end
 end
