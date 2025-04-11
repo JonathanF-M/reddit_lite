ea97209946703d174cd5c1a968ea83394791861f
Jonathan Farias <jfariasm647@gmail.com>
2025-04-11 14:05:12 -0700

    use streams to update the count from server to view

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
   def toggle
+    @votable = params[:votable_type].constantize.find(params[:votable_id])
     Vote.toggle_vote(
-      votable: params[:votable_type].constantize.find(params[:votable_id]),
+      votable: @votable,
       user: current_user,
       vote_type: params[:vote_type].to_i
     )
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: root_path }
+    end
   end
 end
