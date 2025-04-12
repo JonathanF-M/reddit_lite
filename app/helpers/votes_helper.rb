769bd075fdce0f384408c584039a593911a1dff2
Jonathan Farias <jfariasm647@gmail.com>
2025-04-11 18:04:50 -0700

    style buttons on vote

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,12 +1,12 @@
 module VotesHelper
   def votes_color(post, user)
-    return "" unless user
+    return "btn-secondary" unless user
     if user.voted_on?(post, 1)
       "bg-warning"
     elsif user.voted_on?(post, -1)
       "bg-primary"
     else
-      ""
+      "btn-secondary"
     end
   end
 end
